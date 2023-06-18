d694dae72446a9ddd982107a9e966220e8118e46
RakshithaGokuldas <rgraksh@gmail.com>
2023-06-18 12:12:43 -0700

    waasssup

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! Wassssssuup"', () => {
+      expect(appController.getHello()).toBe('Hello World! Wassssssuup');
     });
   });
 });
